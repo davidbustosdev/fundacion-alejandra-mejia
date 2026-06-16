f7ce0a0170e2ab13f8d56fa82f1828d1f258f829
David Bustos <dabn313@gmail.com>
2026-06-16 12:18:35 -0500

    Primer commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,33 +1,33 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'ProCleaning';
-export const SITE_DESCRIPTION = 'Specialized, efficient, and thorough cleaning services';
-
+export const SITE_TITLE = "Fundación Alejandra Mejía";
+export const SITE_DESCRIPTION =
+  "En Fundación Alejandra Mejía trabajamos para brindar apoyo a familias de escasos recursos, niños con discapacidad, personas con enfermedades huérfanas y comunidades vulnerables, generando al mismo tiempo oportunidades de empleo digno a través de la fabricación y distribución de productos de incienso.";
 
 export const NAV_MENU = [
-    {
-        url: "/",
-        title: "Home"
-    },
-    {
-        url: "#",
-        title: "About us"
-    },
-    {
-        url: "/service",
-        title: "Service"
-    },
-    {
-        url: "/team",
-        title: "Team"
-    },
-    {
-        url: "/blog",
-        title: "Blog"
-    },
-     {
-        url: "/contact",
-        title: "Contact"
-    }
+  {
+    url: "/",
+    title: "Home",
+  },
+  {
+    url: "#",
+    title: "About us",
+  },
+  {
+    url: "/service",
+    title: "Service",
+  },
+  {
+    url: "/team",
+    title: "Team",
+  },
+  {
+    url: "/blog",
+    title: "Blog",
+  },
+  {
+    url: "/contact",
+    title: "Contact",
+  },
 ];
